31cdde534feb79e06824d6027eb9a721e9f47660
saidone <saidone@saidone.org>
2023-06-08 08:55:02 +0200

    add command

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # anvedi
 
-A testbed project using CRAL library for interacting with Alfresco. Compatible with GraalVM.
+A testbed project using CRAL library for interacting with Alfresco and compiles down to native code using GraalVM.
 
 ### Build
 Get the sources:
@@ -28,6 +28,22 @@ Finished generating '/home/saidone/anvedi/target/uberjar/anvedi' in 1m 55s.
 Created native image /home/saidone/anvedi/target/uberjar/anvedi
 ```
 and copy the executable binary (target/uberjar/anvedi) somewhere in your path.
+
+Faaast startup!
+```console
+$ time java -jar target/uberjar/anvedi-0.1.0-SNAPSHOT-standalone.jar ticket -u admin -p admin
+TICKET_073f37c8ff6560a97384b2211485ce93115abebf
+
+real    0m1.104s
+user    0m3.109s
+sys     0m0.073s
+$ time target/uberjar/anvedi ticket -u admin -p admin
+TICKET_073f37c8ff6560a97384b2211485ce93115abebf
+
+real    0m0.090s
+user    0m0.005s
+sys     0m0.010s
+```
 ## License
 Copyright (c) 2023 Saidone
 
--- src/anvedi/core.clj
+++ src/anvedi/core.clj
@@ -2,23 +2,27 @@
   (:require
     [anvedi.config :as c]
     [anvedi.utils :as utils]
-    [cral.alfresco.config :as config]
+    [cli-matic.core :refer [run-cmd]]
     [cral.alfresco.auth :as auth]
-    [cral.alfresco.core.nodes :as nodes]
-    [cli-matic.core :refer [run-cmd]])
+    [cral.alfresco.config :as config]
+    [cral.alfresco.core.nodes :as nodes])
   (:gen-class))
 
-(defn- exit [status msg]
+(defn- exit
+  [status msg]
   (println msg)
   (System/exit status))
 
-(defn get-ticket [parms]
+(defn get-ticket
+  [parms]
   (let [response (auth/create-ticket (:user parms) (:password parms))]
     (if (= 201 (:status response))
       (println (get-in response [:body :entry :id]))
       (exit (:status response) (:message response)))))
 
-(defn list-nodes)
+(defn list-children
+  [parms]
+  (println (nodes/list-node-children {:id (:ticket parms) :user-id (:user parms)} (:parent-id parms))))
 
 ;; cli-matic config
 (def CONFIGURATION
@@ -30,11 +34,12 @@
                :opts        [{:option "user" :short "u" :default :present :type :string}
                              {:option "password" :short "p" :default :present :type :string}]
                :runs        get-ticket}
-              {:command     "ls"
-               :description ["list nodes"]
-               :opts        [{:option "ticket" :short "p" :type string}
-                             {:option "path" :short "p" :default :present :type :string}]
-               :runs        list-nodes}
+              {:command     "children"
+               :description ["list children"]
+               :opts        [{:option "user" :short "u" :type :string}
+                             {:option "ticket" :short "t" :type :string}
+                             {:option "parent-id" :short "p" :default :present :type :string}]
+               :runs        list-children}
               ]})
 
 (defn -main
